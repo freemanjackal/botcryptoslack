f689c68483356b47f039bb97fec385753e5792e5
freeman <freemanjackal@gmail.com>
2020-04-06 15:09:49 -0300

    sqlite db- sqlalquemy

---
+++ model.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tokens.sqlite"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+db = SQLAlchemy(app)
+
+
+class Token(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    team_id = db.Column(db.String, unique=True, nullable=False)
+    token = db.Column(db.String, unique=True, nullable=False)
+
+
+def init_db():
+	db.create_all()
+
+def insert_token(team_id, token):
+	db.session.add(Token(team_id=team_id, token=token))
+	db.session.commit()
+
+def get_token(team_id):
+	tokens = db.session.query(Token).filter(Token.team_id==team_id)
+	token = tokens.first()
+	return token.token
+
+
+
+
+
+if __name__ == "__main__":
+	init_db()
+
+
+#token = get_token('Flaskddsd')
+#print(token)
+
